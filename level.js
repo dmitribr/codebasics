aca8030befb20e006b292cb04bcc49bf34a66739
Dmitrybr <dmiitriib@gmail.ru>
2023-11-06 20:26:58 +0900

    lesson6-10

--- level.js
+++ level.js
@@ -11,3 +11,14 @@ console.log(9780262531961 + 1);
 //#5
 console.log('What Is Dead May Never Die');
 //#6
+console.log(81 / 9);
+//#7
+console.log(6 - -81);
+//#8
+console.log(3 ** 5);
+console.log(-8 / -4);
+//#9
+console.log(8 / 2 + 5 - -3 / 2);
+//#10
+console.log(70 * (3 + 4) / (8 + 2));
+//#11
