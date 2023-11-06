9b08a5dd4c53228ecd6282f07f2fdd0ecda3fc9c
Dmitrybr <dmiitriib@gmail.ru>
2023-11-07 01:05:40 +0900

    lesson11-14

--- level.js
+++ level.js
@@ -22,3 +22,11 @@ console.log(8 / 2 + 5 - -3 / 2);
 //#10
 console.log(70 * (3 + 4) / (8 + 2));
 //#11
+console.log(0.39 * 0.22);
+//#12
+console.log((Infinity + Infinity) / 10);
+//#13
+console.log(NaN + 5);
+//#14
+console.log((5 ** 2) - (3 * 7));
+//#15
